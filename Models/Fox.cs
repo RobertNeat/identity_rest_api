486281dee880e8772f65bace1d1553c83216a085
Robert Sereda <robertgoogl@gmail.com>
2023-12-09 12:04:31 +0100

    Delete Models directory

--- Models/Fox.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab9.Models;
-
-public class Fox
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Loves { get; set; }
-    public int Hates { get; set; }
-}
